50a18fc5ce155ee2a19740036b5bb26580d624fe
rockywu 吴佳雷 <rockywu@anjuke.com>
2014-09-15 12:56:13 +0800

    修改部分内容

--- src/compress.upload.images.js
+++ src/compress.upload.images.js
@@ -48,8 +48,9 @@
             'onFailure',    //文件上传失败时,
             'onComplete',   //文件全部上传完毕时
             'onMessage',    //文件上传时出现报错提示
-            'onCheckFile',  //自定义验证是否多次上传
+            'onCheck',  //自定义验证是否多次上传
         ];
+        this.defBoundary = "--image-someboundary--";
         this.defParams = {
             file : null,        //input file dom对象
             uploadUrl : null,   //上传地址
@@ -57,12 +58,13 @@
             maxHeight : 0,      //图片压缩最大高度像素默认为0，不压缩
             inputName : 'file', //设置默认提交的input name 为file
             imageQuality : 100, //默认图片压缩质量为100%
-            httpBoundary : '--my-file-boundary--', //默认文件上传的分隔线
+            httpBoundary : this.defBoundary, //默认文件上传的分隔线
             async : true,       //是否异步传输 默认为true
         };
         this.msg = {
             101 : '无法获取file控件',
-            102 : '无法获取file文件内容'
+            102 : '无法获取file文件内容',
+            103 : '浏览器无法使用xhr.upload对象'
         };
         this.index = 0;
         this.p = Tools.extend(this.defParams, params);   //统一参数
@@ -70,7 +72,6 @@
         this.p.maxHeight = parseInt(this.p.maxHeight);
         this.filesFilter = [];   //文件过滤器
         this.filesName = [];    //文件名保存器
-        this.defBoundary = "--image-someboundary--";
         this.init();            //初始化回调方法
     }
     CUI.prototype = {
@@ -113,27 +114,126 @@
                 }
                 if(files[k].type  === "image/jpeg") {
                     if(this.p.maxWidth > 0 && this.p.maxHeight > 0) {
-                        this.compressImage(index, files[k]);
+                        this.compressImage(this.index, files[k]);
                     } else {
-                        this.uploading(index, files[k]);
+                        this.uploading(this.index, files[k]);
                     }
                 } else {
-                    this.uploading(index, files[k]);
+                    this.uploading(this.index, files[k]);
                 }
-                this.onSelect(index, file[k]);
+                this.onSelect(this.index, files[k]);
+                this.index++;
             }
 
         },
         /**
          *  图片类型压缩
          */
-        compressImage : function() {
-
+        compressImage : function(index, file) {
+            var self = this,
+                reader = new FileReader(),
+                img = document.createElement('img');
+            reader.readAsDataURL(file);
+            reader.onload = function(e) {
+                img.src = this.result;
+            }
+            img.onload = function() {
+                var width = 0,
+                    height = 0,
+                    base64 = '',
+                    mpImg = new MegaPixImage(file),
+                    orientation = 1, //照片方向值
+                    tmpImg = document.createElement('img');
+                if(img.width < self.p.maxWidth && img.height < self.p.maxHeight) {
+                    width = img.width;
+                    height = img.height;
+                } else {
+                    if(img.width / self.p.maxWidth > img.height / self.p.maxHeight ) {
+                        width = self.p.maxWidth;
+                        height = parseInt(img.height * self.p.maxWidth / img.width);
+                    } else {
+                        width = parseInt(img.width * self.p.maxHeight / img.height);
+                        height = self.p.maxHeight;
+                    }
+                }
+                mpImg.render(tmpImg, {maxWidth: width, maxHeight: height });
+                EXIF.getData(file, function() {
+                    orientation=EXIF.getTag(this,'Orientation');
+                    tmpImg.onload=function(){
+                        var tmpCvs = document.createElement("canvas"),
+                            tmpCtx = tmpCvs.getContext('2d'),
+                            data = '';
+                        Tools.rotate(tmpCvs, tmpImg, width, height, orientation);
+                        if(orientation == 6 || orientation == 8){
+                            data = tmpCtx.getImageData(0, 0, height, width);
+                        } else {
+                            data = tmpCtx.getImageData(0, 0, width, height);
+                        }
+                        self.uploading(index, file, data, self.p.httpBoundary);
+                    }
+                });
+            }
         },
         /**
          *  开始文件上传
          */
-        uploading : function(index, file) {
+        uploading : function(index, file, data, boundary) {
+            var self = this,
+                formData,
+                xhr;
+            if(this.p.async === true) {
+                xhr = new XMLHttpRequest();
+            } else {
+                if(typeof this.xhr === 'undefined') {
+                    this.xhr = new XMLHttpRequest();
+                }
+                xhr = this.xhr;
+            }
+            if(! xhr.upload){
+                this.onMessage(103, this.msg['103']);
+                return false;
+            }
+            // 文件上传中
+            xhr.upload.addEventListener("progress", function(e) {
+                self.onProgress(index, file, e.loaded, e.total);
+            });
+            // 文件上传成功或是失败
+            xhr.onreadystatechange = function(e) {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        self.onSuccess(index, file, xhr.responseText);
+                        self.deleteFile(file);
+                        if (!self.filesFilter.length) {
+                            //全部完毕
+                            self.onComplete();
+                        }
+                    } else {
+                        self.onFailure(index, file, xhr.responseText);
+                    }
+                }
+            };
+            // 开始上传
+            xhr.open("POST", this.p.uploadUrl, this.p.async === false ? false : true );
+            if(typeof data === 'undefined' || data === '') {
+                formData = new FormData();
+                formData.append('file', file);
+                xhr.send(formData);
+            } else {
+                boundary = boundary || this.p.defBoundary;
+                if (XMLHttpRequest.prototype.sendAsBinary === undefined) {
+                    XMLHttpRequest.prototype.sendAsBinary = function(string) {
+                        var bytes = Array.prototype.map.call(string, function(c) {
+                            return c.charCodeAt(0) & 0xff;
+                        });
+                        this.send(new Uint8Array(bytes).buffer);
+                    };
+                }
+                var myEncoder = new JPEGEncoder(this.p.imageQuality),
+                    JPEGImage = myEncoder.encode(data, this.p.imageQuality);
+                    data = JPEGImage.substr(23);    //删除base64头
+                xhr.setRequestHeader('Content-Type', 'multipart/form-data; boundary=' + boundary);
+                xhr.sendAsBinary(['--' + boundary, 'Content-Disposition: form-data; name="' + this.p.inputName + '"; filename="' + file.name + '"', 'Content-Type: ' + file.type, '', atob(data), '--' + boundary + '--'].join('\r\n'));
+            }
 
         },
         /**
@@ -146,7 +246,7 @@
                     bool = true;
                 }
             }
-            if(this.onCheckFile(file) && bool) {
+            if(this.onCheck(file) && bool) {
                 return true;
             } else {
                 this.filesName.push(file.name);
@@ -163,103 +263,8 @@
                 }
             }
         },
-        onCheckFile :function(file) {
+        onCheck :function(file) {
             return true;
-        },
-        _doUpload :function(file, data, boundary) {
-            var self = this,
-                formData,
-                xhr = new XMLHttpRequest();    //初始化xhr对象
-            if(!xhr.upload) {
-                this.onMessage('浏览器无法使用xhr.upload对象');
-                return false;
-            }
-            // 文件上传中
-            xhr.upload.addEventListener("progress", function(e) {
-                self.onProgress(file, e.loaded, e.total);
-            });
-            // 文件上传成功或是失败
-            xhr.onreadystatechange = function(e) {
-                if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
-                        self.onSuccess(file, xhr.responseText);
-                        self.deleteFile(file);
-                        if (!self.filesFilter.length) {
-                            //全部完毕
-                            self.onComplete();
-                        }
-                    } else {
-                        self.onFailure(file, xhr.responseText);
-                    }
-                }
-            };
-            // 开始上传
-            xhr.open("POST", this.params.uploadUrl, true);
-            if(typeof data === 'undefined' || data === '') {
-                formData = new FormData();
-                formData.append('file', file);
-                xhr.send(formData);
-            } else {
-                boundary = boundary || this.params.defBoundary;
-                if (XMLHttpRequest.prototype.sendAsBinary === undefined) {
-                    XMLHttpRequest.prototype.sendAsBinary = function(string) {
-                        var bytes = Array.prototype.map.call(string, function(c) {
-                            return c.charCodeAt(0) & 0xff;
-                        });
-                        this.send(new Uint8Array(bytes).buffer);
-                    };
-                }
-                var myEncoder = new JPEGEncoder(this.params.imageQuality),
-                    JPEGImage = myEncoder.encode(data, this.params.imageQuality);
-                    data = JPEGImage.substr(23);    //删除base64头
-                xhr.setRequestHeader('Content-Type', 'multipart/form-data; boundary=' + boundary);
-                xhr.sendAsBinary(['--' + boundary, 'Content-Disposition: form-data; name="' + this.params.inputName + '"; filename="' + file.name + '"', 'Content-Type: ' + file.type, '', atob(data), '--' + boundary + '--'].join('\r\n'));
-            }
-        },
-        _compressUpload : function(file) {
-            var self = this,
-                reader = new FileReader(),
-                img = document.createElement('img');
-            reader.readAsDataURL(file);
-            reader.onload = function(e) {
-                img.src = this.result;
-            }
-            img.onload = function() {
-                var width = 0,
-                    height = 0,
-                    base64 = '',
-                    mpImg = new MegaPixImage(file),
-                    orientation = 1, //照片方向值
-                    tmpImg = document.createElement('img');
-                if(img.width < self.params.maxWidth && img.height < self.params.maxHeight) {
-                    width = img.width;
-                    height = img.height;
-                } else {
-                    if(img.width / self.params.maxWidth > img.height / self.params.maxHeight ) {
-                        width = self.params.maxWidth;
-                        height = parseInt(img.height * self.params.maxWidth / img.width);
-                    } else {
-                        width = parseInt(img.width * self.params.maxHeight / img.height);
-                        height = self.params.maxHeight;
-                    }
-                }
-                mpImg.render(tmpImg, {maxWidth: width, maxHeight: height });
-                EXIF.getData(file, function() {
-                    orientation=EXIF.getTag(this,'Orientation');
-                    tmpImg.onload=function(){
-                        var tmpCvs = document.createElement("canvas"),
-                            tmpCtx = tmpCvs.getContext('2d'),
-                            data = '';
-                        Tools.rotate(tmpCvs, tmpImg, width, height, orientation);
-                        if(orientation == 6 || orientation == 8){
-                            data = tmpCtx.getImageData(0, 0, height, width);
-                        } else {
-                            data = tmpCtx.getImageData(0, 0, width, height);
-                        }
-                        self.doUpload(file, data, "kfstouch-someboundary");
-                    }
-                });
-            }
         }
     };
 
